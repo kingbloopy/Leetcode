3718f96d21dd638773c08b620af7976788f29218
kingbloopy <86815426+kingbloopy@users.noreply.github.com>
2021-10-20 19:33:31 -0700

    Time: 221 ms (35.70%), Space: 44.9 MB (60.82%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) { 
+    
+    let num = 0; 
+    
+    const roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    
+   for (let i = 0; i < s.length; i++){ 
+       
+       if (roman[s[i]] >= roman[s[i+1]] || i === s.length-1){
+           num += roman[s[i]];
+           } else {
+               num += roman[s[i+1]] - roman[s[i]];
+               i++
+           }
+   }
+    
+    return num;
+};
