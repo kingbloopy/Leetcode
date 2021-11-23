dbcb226c0fb73837795ef2fd6db9eb77dea53237
kingbloopy <86815426+kingbloopy@users.noreply.github.com>
2021-11-22 17:42:13 -0800

    Time: 172 ms (14.19%), Space: 212.1 MB (49.53%) - LeetHub

---
+++ move-zeroes/move-zeroes.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+def move_zeroes(nums)
+    total = nums.count(0)
+    nums.delete(0)
+    total.times do
+       nums << 0 
+    end
+    nums
+end
